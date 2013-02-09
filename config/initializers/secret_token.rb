eb0d038d8afaaae2fa30a1986a7db6efaa796210
Kendrick Arnett <kmarnett@gmail.com>
2013-02-09 14:59:10 -0600

    Initial commit to BitBucket.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commonblock::Application.config.secret_token = '2e4db3e705c47a3650a7d7a24136c889cf3c58e5bf3d799b39f481332a5ea03ec9abc233f865587c1541b5db1b46f1e79bb42ad8803c4c6a1b47f4286030c167'
