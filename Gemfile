f3512a1c2cbbc929b896268664fdad5a8124df98
Kendrick Arnett <kmarnett@gmail.com>
2013-02-09 18:57:36 -0600

    Add tests for User model.

--- Gemfile
+++ Gemfile
@@ -12,6 +12,17 @@ gem 'sqlite3'
 group :development, :test do
   gem 'rspec-rails'
   gem 'annotate'
+  gem 'guard-rspec'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'guard-spork'
+  gem 'spork'
+  gem 'launchy'
+  gem 'rb-fsevent'
+  gem 'growl'
 end
 
 # Gems used only for assets and not required
@@ -19,10 +30,6 @@ end
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,111 @@
 #
 # Table name: users
 #
-#  id          :integer          not null, primary key
-#  name        :string(255)
-#  email       :string(255)
-#  postal_code :string(255)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  postal_code     :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Test User", email: "test@example.com",
+                            password: "foobar", password_confirmation: "foobar") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest)}
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = 'abc' * 50 }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addrs = %w[user@foo.COM a_bc-er@a.b.org my.name@google.jp x+y@bar.baz.cn]
+      addrs.each do |valid_addr|
+        @user.email = valid_addr
+        @user.should be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " "}
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation doesn't match password" do
+    before { @user.password_confirmation = "another password" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is missing" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid password") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+
+    describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid }
+    end
+
+    describe "email address entered in mixed case" do
+      let(:mixed_case_email) { "UPPER@lower.cOm" }
+
+      it "should be downcased when saved" do
+        @user.email = mixed_case_email
+        @user.save
+        @user.reload.email.should == mixed_case_email.downcase
+      end
+    end
+  end
 end
