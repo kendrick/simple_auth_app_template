f449e41b83fe56121fe702627bf6ce839038ecae
Kendrick Arnett <kmarnett@gmail.com>
2013-02-11 21:12:24 -0600

    Initial layout.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,33 +1,33 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+  let(:base_title) { "CommonBlock" }
+  subject { page }
+
   describe "Home page" do
-    before { visit '/static_pages/home' }
+    before { visit root_path }
 
-    it "should have the h1 'CommonBlock'" do
-      page.should have_selector('h1', :text => 'CommonBlock')
-    end
-
-    it "should have the base title" do
-      page.should have_selector('title', :text => 'CommonBlock')
-    end
-
-    it "should not have a custom page title" do
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector('h1', text: 'CommonBlock') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: "| Home") }
   end
-
+  
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+    before { visit help_path }
+    
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      page.should have_content('About')
-    end
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
